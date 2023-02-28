28597c7eb96a0d416ded4d0b5fda5ed32df65865
Victor A <adaltezze@gmail.com>
2023-02-28 10:49:48 +0500

    added application build for hosting on github pages

---
+++ static/js/563.a49fd3c0.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_minimal_material_kit_react=self.webpackChunk_minimal_material_kit_react||[]).push([[563],{36563:function(){}}]);
---
+++ static/js/942.0606bf16.chunk.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_minimal_material_kit_react=self.webpackChunk_minimal_material_kit_react||[]).push([[942],{24654:function(){},52361:function(){},94616:function(){}}]);
